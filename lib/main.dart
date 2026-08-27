2a8a3fd0a62023aeb57d5200f8e72a42cbd41287
ladeshina315593-maker <ladeshina315593@gmail.com>
2026-08-27 10:37:25 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,63 @@ import 'package:google_fonts/google_fonts.dart';
 
 import 'src/themes/theme.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const GrapeGoApp());
+}
 
-class MyApp extends StatelessWidget {
+class GrapeGoApp extends StatelessWidget {
+  const GrapeGoApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'E-Commerce ',
+      title: 'Grape Go',
+      debugShowCheckedModeBanner: false,
+
       theme: AppTheme.lightTheme.copyWith(
+        scaffoldBackgroundColor: const Color(0xFFF8F5FF),
+
+        primaryColor: const Color(0xFFB98BEF),
+
+        colorScheme: AppTheme.lightTheme.colorScheme.copyWith(
+          primary: const Color(0xFFB98BEF),
+          secondary: const Color(0xFFD8BFFF),
+        ),
+
         textTheme: GoogleFonts.mulishTextTheme(
           Theme.of(context).textTheme,
+        ).apply(
+          bodyColor: const Color(0xFF30243D),
+          displayColor: const Color(0xFF30243D),
+        ),
+
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Color(0xFF30243D),
+          ),
         ),
       ),
-      debugShowCheckedModeBanner: false,
+
       routes: Routes.getRoute(),
+
       onGenerateRoute: (RouteSettings settings) {
-        if (settings.name.contains('detail')) {
+        if (settings.name != null &&
+            settings.name.contains('detail')) {
           return CustomRoute<bool>(
-              builder: (BuildContext context) => ProductDetailPage());
-        } else {
-          return CustomRoute<bool>(
-              builder: (BuildContext context) => MainPage());
+            builder: (BuildContext context) =>
+                ProductDetailPage(),
+          );
         }
+
+        return CustomRoute<bool>(
+          builder: (BuildContext context) =>
+              MainPage(),
+        );
       },
-      initialRoute: "MainPage",
+
+      initialRoute: 'MainPage',
     );
   }
 }
