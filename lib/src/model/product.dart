5d703829147edda2955ebc74296abb2bbb6cfd7b
ladeshina315593-maker <ladeshina315593@gmail.com>
2026-08-27 13:22:03 +0100

    Update product.dart

--- lib/src/model/product.dart
+++ lib/src/model/product.dart
@@ -1,10 +1,39 @@
-class Product{
+class Product {
   int id;
-  String name ;
-  String category ;
-  String image ;
-  double price ;
-  bool isliked ;
-  bool isSelected ;
-  Product({this.id,this.name, this.category, this.price, this.isliked,this.isSelected = false,this.image});
+  String name;
+  String category;
+  String image;
+  double price;
+
+  bool isliked;
+  bool isSelected;
+
+  // Product details
+  String description;
+  double rating;
+  int reviewCount;
+  int stock;
+
+  // Available options
+  List<String> sizes;
+  List<String> images;
+
+  Product({
+    this.id,
+    this.name,
+    this.category,
+    this.image,
+    this.price,
+    this.isliked = false,
+    this.isSelected = false,
+    this.description = '',
+    this.rating = 0.0,
+    this.reviewCount = 0,
+    this.stock = 0,
+    this.sizes,
+    this.images,
+  }) {
+    sizes ??= [];
+    images ??= [];
+  }
 }
