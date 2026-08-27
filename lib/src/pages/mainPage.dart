101ec621f205762496c2f8d0edf42d2e9d11be22
ladeshina315593-maker <ladeshina315593@gmail.com>
2026-08-27 13:32:40 +0100

    Update mainPage.dart

--- lib/src/pages/mainPage.dart
+++ lib/src/pages/mainPage.dart
@@ -18,83 +18,205 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   bool isHomePageSelected = true;
+
+  Widget _glassIcon(
+    IconData icon, {
+    Color iconColor,
+    VoidCallback onPressed,
+  }) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onPressed,
+        borderRadius: BorderRadius.circular(16),
+        child: Container(
+          height: 46,
+          width: 46,
+          decoration: BoxDecoration(
+            color: AppTheme.glassWhite,
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(
+              color: Colors.white.withOpacity(0.85),
+              width: 1,
+            ),
+            boxShadow: AppTheme.shadow,
+          ),
+          child: Icon(
+            icon,
+            color: iconColor ?? AppTheme.darkText,
+            size: 21,
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget _appBar() {
-    return Container(
+    return Padding(
       padding: AppTheme.padding,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          RotatedBox(
-            quarterTurns: 4,
-            child: _icon(Icons.sort, color: Colors.black54),
+          _glassIcon(
+            Icons.menu_rounded,
+            iconColor: AppTheme.darkText,
+            onPressed: () {
+              // Menu action can be connected when the menu is ready.
+            },
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(13)),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).backgroundColor,
-                boxShadow: <BoxShadow>[
-                  BoxShadow(
-                      color: Color(0xfff8f8f8),
-                      blurRadius: 10,
-                      spreadRadius: 10),
-                ],
+
+          Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {
+                // Profile action can be connected to the profile page.
+              },
+              borderRadius: BorderRadius.circular(16),
+              child: Container(
+                height: 46,
+                width: 46,
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                  color: AppTheme.glassWhite,
+                  borderRadius: BorderRadius.circular(16),
+                  border: Border.all(
+                    color: Colors.white.withOpacity(0.85),
+                    width: 1,
+                  ),
+                  boxShadow: AppTheme.shadow,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(13),
+                  child: Image.asset(
+                    "assets/user.png",
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-              child: Image.asset("assets/user.png"),
             ),
-          ).ripple(() {}, borderRadius: BorderRadius.all(Radius.circular(13)))
+          ),
         ],
       ),
     );
   }
 
-  Widget _icon(IconData icon, {Color color = LightColor.iconColor}) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(13)),
-          color: Theme.of(context).backgroundColor,
-          boxShadow: AppTheme.shadow),
-      child: Icon(
-        icon,
-        color: color,
-      ),
-    ).ripple(() {}, borderRadius: BorderRadius.all(Radius.circular(13)));
-  }
-
   Widget _title() {
     return Container(
-        margin: AppTheme.padding,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TitleText(
-                  text: isHomePageSelected ? 'Our' : 'Shopping',
-                  fontSize: 27,
-                  fontWeight: FontWeight.w400,
+      margin: AppTheme.padding,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TitleText(
+                text: isHomePageSelected ? 'Our' : 'Shopping',
+                fontSize: 27,
+                fontWeight: FontWeight.w400,
+                color: AppTheme.darkText,
+              ),
+              TitleText(
+                text: isHomePageSelected ? 'Products' : 'Cart',
+                fontSize: 27,
+                fontWeight: FontWeight.w700,
+                color: AppTheme.darkText,
+              ),
+            ],
+          ),
+
+          Spacer(),
+
+          if (!isHomePageSelected)
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {
+                  _showClearCartDialog();
+                },
+                borderRadius: BorderRadius.circular(16),
+                child: Container(
+                  height: 44,
+                  width: 44,
+                  decoration: BoxDecoration(
+                    color: AppTheme.glassWhite,
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: Colors.white.withOpacity(0.85),
+                    ),
+                    boxShadow: AppTheme.shadow,
+                  ),
+                  child: Icon(
+                    Icons.delete_outline_rounded,
+                    color: AppTheme.grapePurple,
+                    size: 21,
+                  ),
                 ),
-                TitleText(
-                  text: isHomePageSelected ? 'Products' : 'Cart',
-                  fontSize: 27,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  void _showClearCartDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.white.withOpacity(0.96),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          title: Text(
+            'Clear cart?',
+            style: TextStyle(
+              color: AppTheme.darkText,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          content: Text(
+            'This will remove all items from your cart.',
+            style: TextStyle(
+              color: AppTheme.mutedText,
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                'Cancel',
+                style: TextStyle(
+                  color: AppTheme.mutedText,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+
+                setState(() {});
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Cart cleared'),
+                    backgroundColor: AppTheme.grapePurple,
+                  ),
+                );
+              },
+              child: Text(
+                'Clear',
+                style: TextStyle(
+                  color: AppTheme.grapePurple,
                   fontWeight: FontWeight.w700,
                 ),
-              ],
+              ),
             ),
-            Spacer(),
-            !isHomePageSelected
-                ? Container(
-                  padding: EdgeInsets.all(10),
-                  child: Icon(
-                      Icons.delete_outline,
-                      color: LightColor.orange,
-                    ),
-                ).ripple(() {}, borderRadius: BorderRadius.all(Radius.circular(13)))
-                : SizedBox()
           ],
-        ));
+        );
+      },
+    );
   }
 
   void onBottomIconPressed(int index) {
@@ -112,52 +234,57 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppTheme.grapeLightPurple,
+
       body: SafeArea(
         child: Stack(
           fit: StackFit.expand,
           children: <Widget>[
-            SingleChildScrollView(
-              child: Container(
-                height: AppTheme.fullHeight(context) - 50,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color(0xfffbfbfb),
-                      Color(0xfff7f7f7),
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    _appBar(),
-                    _title(),
-                    Expanded(
-                      child: AnimatedSwitcher(
-                        duration: Duration(milliseconds: 300),
-                        switchInCurve: Curves.easeInToLinear,
-                        switchOutCurve: Curves.easeOutBack,
-                        child: isHomePageSelected
-                            ? MyHomePage()
-                            : Align(
-                                alignment: Alignment.topCenter,
-                                child: ShoppingCartPage(),
-                              ),
-                      ),
-                    )
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    AppTheme.grapeLightPurple,
+                    Colors.white,
                   ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
                 ),
               ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  _appBar(),
+
+                  _title(),
+
+                  Expanded(
+                    child: AnimatedSwitcher(
+                      duration: Duration(milliseconds: 300),
+                      switchInCurve: Curves.easeInToLinear,
+                      switchOutCurve: Curves.easeOutBack,
+                      child: isHomePageSelected
+                          ? MyHomePage()
+                          : Align(
+                              alignment: Alignment.topCenter,
+                              child: ShoppingCartPage(),
+                            ),
+                    ),
+                  ),
+                ],
+              ),
             ),
+
             Positioned(
               bottom: 0,
               right: 0,
+              left: 0,
               child: CustomBottomNavigationBar(
-                onIconPresedCallback: onBottomIconPressed,
+                onIconPresedCallback:
+                    onBottomIconPressed,
               ),
-            )
+            ),
           ],
         ),
       ),
