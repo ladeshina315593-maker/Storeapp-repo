d56bfe3c8b6644138e046c47958411068973d345
ladeshina315593-maker <ladeshina315593@gmail.com>
2026-08-27 11:59:28 +0100

    Update shopping_cart_page.dart

--- lib/src/pages/shopping_cart_page.dart
+++ lib/src/pages/shopping_cart_page.dart
@@ -9,117 +9,204 @@ class ShoppingCartPage extends StatelessWidget {
   const ShoppingCartPage({Key key}) : super(key: key);
 
   Widget _cartItems() {
-    return Column(children: AppData.cartList.map((x) => _item(x)).toList());
+    return Column(
+      children: AppData.cartList
+          .map((product) => _item(product))
+          .toList(),
+    );
   }
 
   Widget _item(Product model) {
     return Container(
-      height: 80,
+      height: 92,
+      margin: const EdgeInsets.only(bottom: 12),
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.52),
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: Colors.white.withOpacity(0.8),
+          width: 1,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.035),
+            blurRadius: 14,
+            offset: const Offset(0, 6),
+          ),
+        ],
+      ),
       child: Row(
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 1.2,
-            child: Stack(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Container(
-                    height: 70,
-                    width: 70,
-                    child: Stack(
-                      children: <Widget>[
-                        Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: LightColor.lightGrey,
-                                borderRadius: BorderRadius.circular(10)),
-                          ),
-                        ),
-                      ],
-                    ),
+        children: [
+          // Product image
+          Container(
+            width: 76,
+            height: 76,
+            decoration: BoxDecoration(
+              color: LightColor.grapeSoftPurple
+                  .withOpacity(0.25),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Image.asset(
+                model.image,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+
+          const SizedBox(width: 12),
+
+          // Product information
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment:
+                  CrossAxisAlignment.start,
+              children: [
+                TitleText(
+                  text: model.name,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  color: LightColor.darkText,
+                ),
+
+                const SizedBox(height: 5),
+
+                Text(
+                  model.category,
+                  style: const TextStyle(
+                    color: LightColor.mutedText,
+                    fontSize: 11,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
-                Positioned(
-                  left: -20,
-                  bottom: -20,
-                  child: Image.asset(model.image),
-                )
+
+                const SizedBox(height: 6),
+
+                TitleText(
+                  text: '\$${model.price.toString()}',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  color: LightColor.grapePurple,
+                ),
               ],
             ),
           ),
-          Expanded(
-              child: ListTile(
-                  title: TitleText(
-                    text: model.name,
-                    fontSize: 15,
-                    fontWeight: FontWeight.w700,
-                  ),
-                  subtitle: Row(
-                    children: <Widget>[
-                      TitleText(
-                        text: '\$ ',
-                        color: LightColor.red,
-                        fontSize: 12,
-                      ),
-                      TitleText(
-                        text: model.price.toString(),
-                        fontSize: 14,
-                      ),
-                    ],
-                  ),
-                  trailing: Container(
-                    width: 35,
-                    height: 35,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: LightColor.lightGrey.withAlpha(150),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: TitleText(
-                      text: 'x${model.id}',
-                      fontSize: 12,
-                    ),
-                  )))
+
+          // Quantity
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 9,
+              vertical: 7,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.60),
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(
+                color: Colors.white.withOpacity(0.75),
+              ),
+            ),
+            child: Text(
+              'x${model.id}',
+              style: const TextStyle(
+                color: LightColor.darkText,
+                fontSize: 11,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
   Widget _price() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        TitleText(
-          text: '${AppData.cartList.length} Items',
-          color: LightColor.grey,
-          fontSize: 14,
-          fontWeight: FontWeight.w500,
+    return Container(
+      padding: const EdgeInsets.all(18),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.48),
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: Colors.white.withOpacity(0.8),
         ),
-        TitleText(
-          text: '\$${getPrice()}',
-          fontSize: 18,
-        ),
-      ],
+      ),
+      child: Row(
+        mainAxisAlignment:
+            MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment:
+                CrossAxisAlignment.start,
+            children: [
+              TitleText(
+                text:
+                    '${AppData.cartList.length} Items',
+                color: LightColor.mutedText,
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+              ),
+
+              const SizedBox(height: 4),
+
+              const Text(
+                'Total',
+                style: TextStyle(
+                  color: LightColor.darkText,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+
+          TitleText(
+            text: '\$${getPrice()}',
+            fontSize: 20,
+            fontWeight: FontWeight.w800,
+            color: LightColor.grapePurple,
+          ),
+        ],
+      ),
     );
   }
 
   Widget _submitButton(BuildContext context) {
-    return TextButton(
-      onPressed: () {},
-      style: ButtonStyle(
-        shape: MaterialStateProperty.all(
-          RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        ),
-        backgroundColor: MaterialStateProperty.all<Color>(LightColor.orange),
+    return Container(
+      width: double.infinity,
+      height: 54,
+      decoration: BoxDecoration(
+        color: LightColor.grapePurple,
+        borderRadius: BorderRadius.circular(18),
+        boxShadow: [
+          BoxShadow(
+            color: LightColor.grapePurple
+                .withOpacity(0.25),
+            blurRadius: 15,
+            offset: const Offset(0, 7),
+          ),
+        ],
       ),
-      child: Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.symmetric(vertical: 4),
-        width: AppTheme.fullWidth(context) * .75,
-        child: TitleText(
-          text: 'Next',
-          color: LightColor.background,
-          fontWeight: FontWeight.w500,
+      child: TextButton(
+        onPressed: () {
+          // Checkout action will be connected
+          // after we inspect the repository's
+          // checkout/order structure.
+        },
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18),
+          ),
+        ),
+        child: const Text(
+          'Continue',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.w700,
+          ),
         ),
       ),
     );
@@ -127,9 +214,11 @@ class ShoppingCartPage extends StatelessWidget {
 
   double getPrice() {
     double price = 0;
-    AppData.cartList.forEach((x) {
-      price += x.price * x.id;
+
+    AppData.cartList.forEach((product) {
+      price += product.price * product.id;
     });
+
     return price;
   }
 
@@ -138,16 +227,30 @@ class ShoppingCartPage extends StatelessWidget {
     return Container(
       padding: AppTheme.padding,
       child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
         child: Column(
-          children: <Widget>[
+          crossAxisAlignment:
+              CrossAxisAlignment.start,
+          children: [
             _cartItems(),
+
+            const SizedBox(height: 12),
+
             Divider(
+              color: LightColor.divider,
               thickness: 1,
-              height: 70,
+              height: 30,
             ),
+
+            const SizedBox(height: 15),
+
             _price(),
-            SizedBox(height: 30),
+
+            const SizedBox(height: 20),
+
             _submitButton(context),
+
+            const SizedBox(height: 25),
           ],
         ),
       ),
