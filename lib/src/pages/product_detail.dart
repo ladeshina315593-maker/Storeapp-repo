bc5ae0072b125add68344906eac11a8aadf0ff79
ladeshina315593-maker <ladeshina315593@gmail.com>
2026-09-06 21:46:22 +0100

    Update product_detail.dart

--- lib/src/pages/product_detail.dart
+++ lib/src/pages/product_detail.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:pikkx/src/pages/chat_page.dart';
 
 class ProductDetailPage extends StatefulWidget {
   const ProductDetailPage({
